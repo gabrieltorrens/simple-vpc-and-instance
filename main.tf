a0149bf3249f4de837b1cd19e6d0a687da9eba58
gabrieltorrens <50712426+gabrieltorrens@users.noreply.github.com>
2024-10-07 23:13:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "my_vpc1" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
